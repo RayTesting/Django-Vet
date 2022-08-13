3e847bf0c0de3ba53fb4126e2ea109048c1af195
Ray Salgado <19120236@morelia.tecnm.mx>
2022-08-13 15:03:37 -0500

    first deploy configurations

--- core/settings.py
+++ core/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['https://7cf2-2806-103e-1d-d3d0-dc68-5c2d-1cc7-5251.ngrok.io']
+ALLOWED_HOSTS = ['django-vet.herokuapp.com','127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://django-vet.herokuapp.com']
 
 
 # Application definition
@@ -201,6 +201,7 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -282,6 +283,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR, "static")
